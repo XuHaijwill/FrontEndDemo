a7fdb107a90814c7620e0b052cb587bb403fc860
XuHaijwill <zuiwoxuanlou@163.com>
2024-01-22 09:56:31 +0800

    code commit

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,27 +1,21 @@
+// 受控绑定表单
+
+import { useState } from "react"
+
+// 1. 声明一个react状态 - useState
+
+// 2. 核心绑定流程
+// 1. 通过value属性绑定react状态
+// 2. 绑定onChange事件 通过事件参数e拿到输入框最新的值 反向修改到react状态身上
 
 function App () {
-  // 基础绑定
-  // const handleClick = () => {
-  //   console.log('button被点击了')
-  // }
-
-  // 事件参数e
-  // const handleClick = (e) => {
-  //   console.log('button被点击了', e)
-  // }
-
-  // 传递自定义参数
-  // const handleClick = (name) => {
-  //   console.log('button被点击了', name)
-  // }
-
-  // 既要传递自定义参数 而且还要事件对象e
-  const handleClick = (name, e) => {
-    console.log('button被点击了', name, e)
-  }
+  const [value, setValue] = useState('')
   return (
-    <div className="App">
-      <button onClick={(e) => handleClick('jack', e)}>click me </button>
+    <div>
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        type="text" />
     </div>
   )
 }
