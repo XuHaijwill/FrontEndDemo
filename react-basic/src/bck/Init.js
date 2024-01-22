56ba6e61930fc1b43d47d53d10065137c56ae919
XuHaijwill <zuiwoxuanlou@163.com>
2024-01-22 09:29:28 +0800

    code commit

---
+++ react-basic/src/bck/Init.js
@@ -0,0 +1,13 @@
+//项目根组件
+//App -> index.js -> public/index.html(root)
+
+const message='this is App'
+function App() {
+  return (
+    <div className="App">
+        {message}
+    </div>
+  );
+}
+
+export default App;
